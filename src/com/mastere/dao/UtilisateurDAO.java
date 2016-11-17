ba6814f98d7b011bbc154f702dafc0e0bddab6ee
Adrien-mac <Adrien@MacBook-Pro-de-Adrien-mac.local>
2016-11-17 12:15:14 +0100

    first commit

--- src/com/mastere/dao/UtilisateurDAO.java
+++ src/com/mastere/dao/UtilisateurDAO.java
@@ -10,9 +10,9 @@ public class UtilisateurDAO {
 	
 	public static Utilisateur getByMailPassword(final String mail, final String password){
 		Utilisateur u =  (Utilisateur) HibernateUtil.getSession()
-				.createQuery("from Utilisateur where mail=?mail and password=?pass")
-				.setParameter(1, mail)
-				.setParameter(2, password).getSingleResult();//uniqueResult();
+				.createQuery("from Utilisateur where mail=? and password=?")
+				.setParameter(0, mail)
+				.setParameter(1, password).getSingleResult();//uniqueResult();
 /*		List<Utilisateur> result = (List<Utilisateur>)  HibernateUtil.getSession()
 				.createQuery("from Utilisateur where mail=?mail and password=?pass").list();
 		Utilisateur toto = (Utilisateur) HibernateUtil.getSession()
